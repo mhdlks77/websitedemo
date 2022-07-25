481fd67d4aaa762947eabf3276048c29c9377220
oneblindguardian <44147836+oneblindguardian@users.noreply.github.com>
2022-07-24 20:00:38 -0400

    Update ItemController.java

--- website_demo/website_demo/src/main/java/com/example/website_demo/Item/ItemController.java
+++ website_demo/website_demo/src/main/java/com/example/website_demo/Item/ItemController.java
@@ -2,6 +2,22 @@ package com.example.website_demo.Item;
 
 import org.springframework.stereotype.Controller;
 
-@Controller
+@RestController
 public class ItemController {
+  
+  @AutoWired
+  private ItemRepository itemRepository;
+  
+  @GetMapping 
+  public Item getItem(){
+    var item = itemRepository.findAll();
+    return item;
+  }
+  
+  @PostMapping
+  public void saveItem(Item item){
+    itemRepository.save(item);
+    
+  }
+  
 }
